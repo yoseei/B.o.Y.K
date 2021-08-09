9a73073f3415cb895fb3221d4cf1a24ecc7ab847
Yoshitaka Kai <katashiyoika@gmail.com>
2021-08-09 21:22:07 +0900

    createBlog実装

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import scss from "./App.module.scss";
 import Footer from "./components/footer/Footer";
 import BlogForm from "./features/blog/blogForm/BlogForm";
 import PostsDetail from "./components/main/PostsDetail";
-import PostsEdit from "./components/main/PostsEdit";
+import BlogEdit from "./features/blog/blogEdit/BlogEdit";
 import DeleteModal from "./components/UIKit/DeleteModal";
 import BlogList from "./features/blog/blogList/BlogList";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
@@ -30,7 +30,7 @@ function App() {
         <div className={scss.main}>
           <Switch>
             <Route exact path="/create" component={BlogForm} />
-            <Route exact path="/edit" component={PostsEdit} />
+            <Route exact path="/edit" component={BlogEdit} />
             <Route exact path="/detail/:id" component={PostsDetail} />
             <Route exact path="/list" component={BlogList} />
 
